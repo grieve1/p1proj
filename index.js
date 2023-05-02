2c7811ca8d6610d496a78fadbf7f9a0d805042c7
drake bishop <drakemx85@gmail.com>
2023-05-02 03:35:56 -0500

    changed the alignment of the games list

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ searchButton.addEventListener('click', e => {
     fetch(`https://www.cheapshark.com/api/1.0/games?title=${selectedGenre}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        // code to display the search results
+        console.log(data); // just for testing
+        // sort the games alphabetically
         data.sort((a, b) => a.external.localeCompare(b.external));
+        // code to display the search results
         searchResults.innerHTML = ''; // clear previous results
         for (const game of data) {
           const gameRow = document.createElement('div');
@@ -35,12 +36,7 @@ searchButton.addEventListener('click', e => {
           gameRow.appendChild(price);
           
           const buyLink = document.createElement('a');
-          const steamStoreID = 1;
-          if (game.steamAppID !== null) {
-            buyLink.href = `https://store.steampowered.com/app/${game.steamAppID}/?utm_source=cheapshark&utm_medium=referral&utm_campaign=cheapshark`;
-          } else {
-            buyLink.href = game.link;
-          }
+          buyLink.href = `${game.link}&storeID=1`;
           buyLink.target = '_blank';
           buyLink.innerText = 'Buy now';
           gameRow.appendChild(buyLink);
